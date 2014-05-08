059c3d6cc9b30bdc285df611f5687b7f5874bbca
Xiyang Dai <vdavid70619@gmail.com>
2014-05-08 11:40:37 -0400

    kmers

---
+++ k_mer_features.m
@@ -0,0 +1,21 @@
+function hist = k_mer_features(data, K)
+    hist = [];
+    for j=1:length(K)
+        k = K(j)
+        onefilter = ones(k,1);
+        k_mers = imfilter(data, onefilter, 'conv');
+        k_mers = k_mers(:, ceil(k/2):end-ceil(k/2));
+
+        k_ind = unique(k_mers);
+        k_ind = sort(k_ind);
+
+        hist1 = zeros(size(data,1),length(k_ind));
+        for i=1:length(k_ind)
+            hist1(:,i) = sum(k_mers==k_ind(i), 2);
+        end
+        hist1 = hist1/sum(hist1);
+        
+        hist = [hist hist1];
+    end
+    hist;
+end
