84eec2561422afba8edc181fed4187864de8a584
U-Dai7-PC\Dai7 <Dai7@DAI7-PC.(none)>
2014-05-10 19:33:45 -0400

    Random repeat test cases many times for lassso to avoid overfitting :(

--- dimension_reduct.m
+++ dimension_reduct.m
@@ -4,16 +4,22 @@ function dimension_reduct()
     test = load('test_cache.mat');
 
     train_samples = cat(2, train.data.feature{:})'; 
-    test_samples = cat(2, test.data.feature{:})'; 
-
-%     train_labels = train.data.population;
-%     test_labels = test.data.population;
-    
     train_labels = train.data.label;
-    test_labels = ones(size(test_samples,1),1);
-
-    samples = [train_samples; test_samples];
-    labels = [train_labels; test_labels];
+ 
+    test_samples = cat(2, test.data.feature{:})';     
+    n=20
+    repeat_test_labels=[];
+    repeat_test_samples=[];
+    for i=1:n
+        repeat_test_labels = [repeat_test_labels; rand(size(test_samples,1),1)];
+        repeat_test_samples = [repeat_test_samples; test_samples];
+    end
+    repeat_test_labels(repeat_test_labels>0.5) = 1;
+    repeat_test_labels(repeat_test_labels<=0.5) = 0;
+    fprintf('Summary for generate random labels: %d/%d', sum(repeat_test_labels), length(repeat_test_labels));
+    
+    samples = [train_samples; repeat_test_samples];
+    labels = [train_labels; repeat_test_labels];
     
     lscores = lasso(samples, labels);
     [~, ind] = sort(sum(abs(lscores),2), 'descend');
